46229ebfbe4e7eed04696acdbf32d28a779bac12
0wen11 <owensidarta@gmail.com>
2024-11-29 21:14:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return <div className='bg-orange-800	'>Hello World!</div>
+}
+
+export default App
